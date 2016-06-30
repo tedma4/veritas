702630ab09ca88ccd8ff6c8a283c3e1ee1305707
ted martinez <tedmartinez@ted.local>
2016-06-29 19:20:40 -0700

    Removed pry

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -17,7 +17,6 @@ class Api::V1::ImagesController < ApplicationController
     # When Images need to be polymorphic, uncomment below
     # @imageable.attachments.create image_params
     # redirect_to @imageable
-    binding.pry
     @image = Image.new(image_params)
 
     respond_to do |format|
