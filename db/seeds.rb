5cf962d497655fb94a93654ef4a70df3f41fce21
ted martinez <tedmartinez@ted.local>
2016-10-12 18:45:51 -0700

    Updated seed file for heroku image test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,26 @@
 # unless User.any? 
-  200.times do |n|
-    user = User.new
-    user.first_name = Faker::Name.first_name
-    user.last_name = Faker::Name.last_name
-  	user.user_name = Faker::Internet.user_name
-  	user.email = Faker::Internet.email
-  	user.password =  'password'
-  	user.password_confirmation =  'password'
-  	user.current_location = [Faker::Address.longitude, Faker::Address.latitude] 
-    user.remote_avatar_url = Faker::Avatar.image
-    # user.validate = false
-    user.save(validate: false)
-    user.create_pin
-  end
+  # 200.times do |n|
+  #   user = User.new
+  #   user.first_name = Faker::Name.first_name
+  #   user.last_name = Faker::Name.last_name
+  # 	user.user_name = Faker::Internet.user_name
+  # 	user.email = Faker::Internet.email
+  # 	user.password =  'password'
+  # 	user.password_confirmation =  'password'
+  # 	user.current_location = [Faker::Address.longitude, Faker::Address.latitude] 
+  #   user.remote_avatar_url = Faker::Avatar.image
+  #   # user.validate = false
+  #   user.save(validate: false)
+  #   user.create_pin
+  # end
 
-  if User.where(:followed_users.eq => []).any?
-    User.where(:followed_users.eq => []).each do |user|
-      user.followed_users << User.sample(10).pluck(:id)
-      user.followed_users.flatten!
-      user.save
-    end
-  end
+  # if User.where(:followed_users.eq => []).any?
+  #   User.where(:followed_users.eq => []).each do |user|
+  #     user.followed_users << User.sample(10).pluck(:id)
+  #     user.followed_users.flatten!
+  #     user.save
+  #   end
+  # end
 # end
 # unless Post.any?
   800.times do |i|
@@ -31,13 +31,13 @@
     post.save
   end
 
-100.times do |i|
-  note = Notification.new
-  note.user_id = User.sample.id
-  note.notified_by_id = User.sample.id
-  note.notice_type = ["accept request", "friend request", "pin signup"].sample
-  note.save
-end
+# 1000.times do |i|
+#   note = Notification.new
+#   note.user_id = User.sample.id
+#   note.notified_by_id = User.sample.id
+#   note.notice_type = ["accept request", "friend request", "pin signup"].sample
+#   note.save
+# end
 
 # end
 
