8fccc02afba265aaef8ea4f825d5835dec7e6853
Ted <tedma4@yahoo.com>
2016-11-26 10:29:03 -0700

    Forgot to add indexes to likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,8 +2,8 @@ class Like
   include NoBrainer::Document
   include NoBrainer::Document::Timestamps
 
-  belongs_to :post
-  belongs_to :user
+  belongs_to :post, index: true
+  belongs_to :user, index: true
   validates :user_id, uniqueness: { scope: :post_id }
 
 end
